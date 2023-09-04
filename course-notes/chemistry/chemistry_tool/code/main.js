f0043da1d2ad88e40214a1db71b85bd3c4f1ffb0
Tyrcnex <80244419+Tyrcnex@users.noreply.github.com>
2023-09-04 15:01:11 +0800

    pushie

--- course-notes/chemistry/chemistry_tool/code/main.js
+++ course-notes/chemistry/chemistry_tool/code/main.js
@@ -1,13 +1,30 @@
 let chemistryInput = document.getElementById("chemistryInput");
 let output = document.getElementById("output");
+let dataSelect = document.getElementById("dataSelect");
 
 let parser = new Parser("");
 
-chemistryInput.addEventListener("input", event => {
-    output.value = "ERR";
-    parser.updateInput(event.target.value);
-    let parseOutput = parser.parse(1);
-    if (parseOutput.className) {
-        output.value = JSON.stringify(parseOutput.getMolarMass(), null, 4);
-    } else output.value = "";
-})
+function out(event) {
+    output.value = "ERROR! (will improve error detection soon)";
+    parser.updateInput(chemistryInput.value);
+    let parseOutput = parser.parse();
+    let outObj;
+    switch (dataSelect.value) {
+        case "data_structure": {
+            outObj = parseOutput;
+            break;
+        }
+        case "count": {
+            outObj = parseOutput.count();
+            break;
+        }
+        case "atomic_mass": {
+            outObj = parseOutput.getMolarMass();
+            break;
+        }
+    }
+    output.value = JSON.stringify(outObj, null, 4);
+}
+
+chemistryInput.addEventListener("input", out);
+dataSelect.onchange = out;
